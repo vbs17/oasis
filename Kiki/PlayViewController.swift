8745a50868597bd1b6a002647d33377dde97919b
kei ikeuchi <sister@sister.local>
2016-10-13 21:40:35 +0900

    why

--- Kiki/PlayViewController.swift
+++ Kiki/PlayViewController.swift
@@ -6,35 +6,54 @@ import AVFoundation
 class PlayViewController: UIViewController {
     
     var songData:NSURL!
-    var timeCount = 1
-    var soundIdRing:SystemSoundID = 0
+    var playSong:AVAudioPlayer!
+    let deleteSong:AVAudioRecorder!
     
     @IBOutlet weak var play: UIButton!
-    @IBOutlet weak var pause: UIButton!
     @IBOutlet weak var back: UIButton!
     @IBOutlet weak var byou: UILabel!
-    @IBOutlet weak var onbyou: UILabel!
+    
     
     //秒数がすでに表示
     override func viewDidLoad() {
         super.viewDidLoad()
         let sound:AVAudioPlayer = try! AVAudioPlayer(contentsOfURL: songData!)
+        playSong = sound
         byou.text = sound.duration.description
+        sound.prepareToPlay()
+        byou.text = formatTimeString(sound.duration)
+        play.layer.cornerRadius = 37
+        play.clipsToBounds = true
+        back.layer.cornerRadius = 37
+        back.clipsToBounds = true
     }
     
     //再生
     @IBAction func goPlay(sender: AnyObject) {
-        AudioServicesCreateSystemSoundID(songData!, &soundIdRing)
-        AudioServicesPlaySystemSound(soundIdRing)
+        playSong.play()
+        play.enabled = false
+        back.enabled = true
     }
-    //一時停止
-    @IBAction func goPause(sender: AnyObject) {
-
-    }
-    
     //巻き戻し
-    @IBAction func goBack(sender: AnyObject) {
+    @IBAction func goBack(sender: UIButton) {
+        play.enabled = true
+        back.enabled = false
+        playSong.stop()
+        playSong.prepareToPlay()
+        playSong.currentTime = 0
+    }
     
+    @IBAction func retake(sender: AnyObject) {
+        deleteSong.url =
+        deleteSong.deleteRecording()
+    }
+    
+    
+    func formatTimeString(d: Double) -> String {
+        let s: Int = Int(d % 60)
+        let m: Int = Int((d - Double(s)) / 60 % 60)
+        let str = String(format: "%2d:%02d",  m, s)
+        return str
     }
         
     
