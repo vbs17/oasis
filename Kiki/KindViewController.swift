1ef4d4a5a63537c3e756b4465dc0c8787934711e
kei ikeuchi <sister@sister.local>
2016-10-16 18:12:18 +0900

    h

--- Kiki/KindViewController.swift
+++ Kiki/KindViewController.swift
@@ -452,28 +452,7 @@ class KindViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Status Barの高さを取得を.する.
-        let barHeight: CGFloat = UIApplication.sharedApplication().statusBarFrame.size.height
-        
-        // Viewの高さと幅を取得する.
-        let displayWidth: CGFloat = self.view.frame.width
-        let displayHeight: CGFloat = self.view.frame.height
-        
-        // TableViewの生成( status barの高さ分ずらして表示 ).
-        let myTableView: UITableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
-        
-        // Cell名の登録をおこなう.
-        myTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
-        
-        // DataSourceの設定をする.
-        myTableView.dataSource = self
-        
-        // Delegateを設定する.
-        myTableView.delegate = self
-        
-        // Viewに追加する.
-        self.view.addSubview(myTableView)
+       
     }
     
     override func didReceiveMemoryWarning() {
