ab1b22a791a6631686ec67ade079c2acb6e59a70
kei ikeuchi <sister@sister.local>
2016-10-23 19:06:40 +0900

    a

--- Kiki/HomeViewController.swift
+++ Kiki/HomeViewController.swift
@@ -15,6 +15,7 @@ class HomeViewController: UIViewController,UITableViewDataSource, UITableViewDel
     var back: UIButton!
     var tableView: UITableView!
     var playingIndexPath:NSIndexPath!
+
     
     override func viewDidLoad() {
         
@@ -28,6 +29,54 @@ class HomeViewController: UIViewController,UITableViewDataSource, UITableViewDel
         //各ユーザの平均評価が表示（例）１０人の評価の平均を星の色に反映させる
         
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if FIRAuth.auth()?.currentUser != nil {
+            if observing == false {
+                FIRDatabase.database().reference().child(CommonConst.PostPATH).observeEventType(.ChildAdded, withBlock: { snapshot in
+                    
+                    if let uid = FIRAuth.auth()?.currentUser?.uid {
+                        let postData = PostData(snapshot: snapshot, myId: uid)
+                        self.postArray.insert(postData, atIndex: 0)
+                        
+                        self.tableView.reloadData()
+                    }
+                })
+                
+                FIRDatabase.database().reference().child(CommonConst.PostPATH).observeEventType(.ChildChanged, withBlock: { snapshot in
+                    if let uid = FIRAuth.auth()?.currentUser?.uid {
+                        let postData = PostData(snapshot: snapshot, myId: uid)
+                        
+                        var index: Int = 0
+                        for post in self.postArray {
+                            if post.id == postData.id {
+                                index = self.postArray.indexOf(post)!
+                                break
+                            }
+                        }
+                        
+                        self.postArray.removeAtIndex(index)
+                        
+                        self.postArray.insert(postData, atIndex: index)
+                        
+                        self.tableView.reloadData()
+                    }
+                })
+                
+                observing = true
+            }
+        } else {
+            if observing == true {
+                postArray = []
+                tableView.reloadData()
+                FIRDatabase.database().reference().removeAllObservers()
+                observing = false
+            }
+        }
+    }
+    
+
     
     
     func hyoukaGo(){
@@ -89,6 +138,12 @@ class HomeViewController: UIViewController,UITableViewDataSource, UITableViewDel
     
     
     
+    
+    
+
+    
+    
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return postArray.count
     }
@@ -107,6 +162,7 @@ class HomeViewController: UIViewController,UITableViewDataSource, UITableViewDel
         cell.setPostData(postArray[indexPath.row])
         cell.playButton.addTarget(self, action:#selector(handleButton(_:event:)), forControlEvents: UIControlEvents.TouchUpInside)
         cell.backButton.addTarget(self, action:#selector(back(_:event:)), forControlEvents: UIControlEvents.TouchUpInside)
+        cell.hyouka.addTarget(self, action: #selector(hoshi(_:event:)), forControlEvents: UIControlEvents.TouchUpInside)
         return cell
     }
 
@@ -188,27 +244,12 @@ class HomeViewController: UIViewController,UITableViewDataSource, UITableViewDel
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
     
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        if FIRAuth.auth()?.currentUser != nil {
-            if observing == false {
-                // 写真　曲名　秒数　音源が追加されたらpostArray(写真　曲名　秒数　音源の格納庫)に追加してTableViewを再表示する
-                FIRDatabase.database().reference().child(CommonConst.PostPATH).child(genre).observeEventType(.ChildAdded, withBlock: { snapshot in
-                    
-                    // PostDataクラスを生成して受け取ったデータを設定する
-                    if let uid = FIRAuth.auth()?.currentUser?.uid {
-                        let postData = PostData(snapshot: snapshot, myId: uid) //写真　曲名　秒数　音源が存在してる
-                        self.postArray.insert(postData, atIndex: 0)
-                        
-                        // TableViewを再表示する
-                        self.tableView.reloadData()
-                    }
-                })
-            }
-        }
-    }
-
     
+
+
+
+
+
     @IBAction func backGo(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
