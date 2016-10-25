9d992dff26ced7276a804cdfc1eca7da7fd677a8
kei ikeuchi <sister@sister.local>
2016-10-25 21:18:30 +0900

    m

--- HomeTableViewCell.swift
+++ HomeTableViewCell.swift
@@ -30,23 +30,40 @@ class HomeTableViewCell: UITableViewCell {
     //星の色　ここはシンプル 黄色
     func updateStar(ratingStr:Int) {
         switch  ratingStr {
+        case 0:
+            star1.setImage(UIImage(named:"IMG_2728 2"), forState: UIControlState.Normal)
+            star2.setImage(UIImage(named:"IMG_2728 2"), forState: UIControlState.Normal)
+            star3.setImage(UIImage(named:"IMG_2728 2"), forState: UIControlState.Normal)
+            star4.setImage(UIImage(named:"IMG_2728 2"), forState: UIControlState.Normal)
+            star5.setImage(UIImage(named:"IMG_2728 2"), forState: UIControlState.Normal)
         case 1:
             star1.setImage(UIImage(named:"IMG_2727 2"), forState: UIControlState.Normal)
+            star2.setImage(UIImage(named:"IMG_2728 2"), forState: UIControlState.Normal)
+            star3.setImage(UIImage(named:"IMG_2728 2"), forState: UIControlState.Normal)
+            star4.setImage(UIImage(named:"IMG_2728 2"), forState: UIControlState.Normal)
+            star5.setImage(UIImage(named:"IMG_2728 2"), forState: UIControlState.Normal)
             
         case 2:
             star1.setImage(UIImage(named:"IMG_2727 2"), forState: UIControlState.Normal)
             star2.setImage(UIImage(named:"IMG_2727 2"), forState: UIControlState.Normal)
+            star3.setImage(UIImage(named:"IMG_2728 2"), forState: UIControlState.Normal)
+            star4.setImage(UIImage(named:"IMG_2728 2"), forState: UIControlState.Normal)
+            star5.setImage(UIImage(named:"IMG_2728 2"), forState: UIControlState.Normal)
 
         case 3:
             star1.setImage(UIImage(named:"IMG_2727 2"), forState: UIControlState.Normal)
             star2.setImage(UIImage(named:"IMG_2727 2"), forState: UIControlState.Normal)
             star3.setImage(UIImage(named:"IMG_2727 2"), forState: UIControlState.Normal)
+            star4.setImage(UIImage(named:"IMG_2728 2"), forState: UIControlState.Normal)
+            star5.setImage(UIImage(named:"IMG_2728 2"), forState: UIControlState.Normal)
             
         case 4:
             star1.setImage(UIImage(named:"IMG_2727 2"), forState: UIControlState.Normal)
             star2.setImage(UIImage(named:"IMG_2727 2"), forState: UIControlState.Normal)
             star3.setImage(UIImage(named:"IMG_2727 2"), forState: UIControlState.Normal)
             star4.setImage(UIImage(named:"IMG_2727 2"), forState: UIControlState.Normal)
+            star5.setImage(UIImage(named:"IMG_2728 2"), forState: UIControlState.Normal)
+
         case 5:
             star1.setImage(UIImage(named:"IMG_2727 2"), forState: UIControlState.Normal)
             star2.setImage(UIImage(named:"IMG_2727 2"), forState: UIControlState.Normal)
