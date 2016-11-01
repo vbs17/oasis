8e1214411d0d557e7a6a370b07bf49c7541584df
kei ikeuchi <sister@sister.local>
2016-11-01 18:03:20 +0900

    m

--- Kiki/TapliViewController.swift
+++ Kiki/TapliViewController.swift
@@ -1,26 +1,31 @@
 
 
 import UIKit
+extension String {
+    
+    func escapeStr() -> String {
+        var raw: NSString = self
+        var str = CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault,raw,"[].",":/?&=;+!@#$()',*",CFStringConvertNSStringEncodingToEncoding(NSUTF8StringEncoding))
+        return str as String
+    }
+}
 
 class TapliViewController: UIViewController {
     
     var path1:String!
+    var encoded =  ""
 
     @IBOutlet weak var path: UITextView!
-    
-    
-    
-    
-    
     @IBAction func map(sender: AnyObject) {
-        func schemebtn(sender: AnyObject) {
-            let url = NSURL(string: "http://maps.apple.com/?q")!
-            if (UIApplication.sharedApplication().canOpenURL(url)) {
-                UIApplication.sharedApplication().openURL(url)
+        schemebtn()
             }
+    
+    func schemebtn() {
+        let url = NSURL(string: "http://maps.apple.com/?q=\(encoded)")!
+        if (UIApplication.sharedApplication().canOpenURL(url)) {
+            UIApplication.sharedApplication().openURL(url)
         }
     }
-    
     
     
     
@@ -31,7 +36,9 @@ class TapliViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         path.text = path1
+        var encoded = path1.escapeStr()
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
