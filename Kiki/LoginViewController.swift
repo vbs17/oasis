6cd27f71929151b6bd0f315d2eb5b367552e266c
kei ikeuchi <sister@sister.local>
2016-11-09 18:59:16 +0900

    d

--- Kiki/LoginViewController.swift
+++ Kiki/LoginViewController.swift
@@ -3,11 +3,10 @@ import UIKit
 import Firebase
 import FirebaseAuth
 import SVProgressHUD
-import FBSDKCoreKit
-import FBSDKLoginKit
 
 
-class LoginViewController: UIViewController,FBSDKLoginButtonDelegate {
+
+class LoginViewController: UIViewController{
     @IBOutlet weak var mailAddressTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var displayNameTextField: UITextField!
@@ -103,42 +102,8 @@ class LoginViewController: UIViewController,FBSDKLoginButtonDelegate {
    
     
     
-    override func viewDidAppear(animated: Bool) {
-        if (FBSDKAccessToken.currentAccessToken() != nil) {
-            print("User Already Logged In")
-            //後で既にログインしていた場合の処理（メイン画面へ遷移）を書く
-        } else {
-            print("User not Logged In")
-            let loginView : FBSDKLoginButton = FBSDKLoginButton()
-            self.view.addSubview(loginView)
-            loginView.center = self.view.center
-            loginView.readPermissions = ["public_profile", "email", "user_friends"]
-            loginView.delegate = self
-        }
-    }
     
-    
-    
-    @IBAction func facebooklog(sender: AnyObject) {
-        
-        func loginButton(loginButton: FBSDKLoginButton!,didCompleteWithResult
-            result: FBSDKLoginManagerLoginResult!, error: NSError!) {
-            print("User Logged In")
-            
-            if ((error) != nil)
-            {
-                //エラー処理
-            } else if result.isCancelled {
-                //キャンセルされた時
-            } else {
-                //必要な情報が取れていることを確認(今回はemail必須)
-                if result.grantedPermissions.contains("email")
-                {
-                    // 次の画面に遷移（後で）
-                }
-            }
-        }
-
+   
         
     }
     
@@ -207,4 +172,3 @@ class LoginViewController: UIViewController,FBSDKLoginButtonDelegate {
     
     
     
-}
